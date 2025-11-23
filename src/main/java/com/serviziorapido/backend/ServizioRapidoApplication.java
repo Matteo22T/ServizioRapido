44c9c15c58030c1ea91fb14703b6fe456f8a92f0
Matteo Tocci <matteo.tocci2004@gmail.com>
2025-11-23 16:16:20 +0100

    Inizio Progetto

---
+++ src/main/java/com/serviziorapido/backend/ServizioRapidoApplication.java
@@ -0,0 +1,13 @@
+package com.serviziorapido.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServizioRapidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServizioRapidoApplication.class, args);
+    }
+
+}
